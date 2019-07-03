b9f5d9245d821b2f4dccb6f2b8de9fad5112952c
Tyler Schaffer <tpschaffer@gmail.com>
2019-07-03 09:13:34 -0600

    Complete room class and test

--- test/room_test.rb
+++ test/room_test.rb
@@ -3,5 +3,20 @@ require 'minitest/pride'
 require './lib/room'
 
 class RoomTest < Minitest::Test
+  def setup
+    @room = Room.new(:bedroom, 10, 13)
+  end
+
+  def test_it_exists
+    assert_instance_of Room, @room
+  end
+
+  def test_attributes
+    assert_equal :bedroom, @room.category
+  end
+
+  def test_area
+    assert_equal 130, @room.area
+  end
 
 end
